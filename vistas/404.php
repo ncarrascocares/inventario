e4ff32398978f8fcfb790ff26206469fec96e45c
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2022-08-01 14:14:49 -0400

    Modificación de los archivos vistas del proyecto, ademas de los estilos css

--- vistas/404.php
+++ vistas/404.php
@@ -1,1 +1,12 @@
-404
+<div class="main-container">
+    <section class="hero-body">
+	  	<div class="hero-body">
+		    <p class="title">
+		      Error 404
+		    </p>
+		    <p class="subtitle">
+		      Pagina no encontrada
+		    </p>
+	  	</div>
+	</section>
+</div>
