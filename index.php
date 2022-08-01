f8e6d8757ba1be4df670ca95e087a9146cdb0e4f
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2022-08-01 12:50:49 -0400

    Inclusión de condicionales para seleccion de las vistas

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    require "./inc/session_start.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php
@@ -5,9 +9,26 @@
 ?>
 <body>
     <?php
-        include "./inc/navbar.php";
 
-        include "./inc/script.php";
+        //Condicional para verificar si la veriable tipo GET viene definida o vacia
+        if (!isset($_GET['vista']) || $_GET['vista']=="") {
+            $_GET['vista'] ="login";
+        }
+
+        //carga de una vista dependiendo de lo que contenga el metodo GET
+        if (is_file("./vistas/" .$_GET['vista'].".php") && $_GET['vista'] !="login" && $_GET['vista'] !="404") {
+
+            include "./inc/navbar.php";
+            include "./vistas/".$_GET['vista'].".php";
+            include "./inc/script.php";
+
+        }else{
+            if ($_GET['vista']=="login") {
+                include "./vistas/login.php";
+            }else{
+                include "./vistas/404.php";
+            }
+        }
     ?>
 </body>
 </html>
