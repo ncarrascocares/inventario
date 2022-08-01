33dbb573725a81f32335a8f79401b0cf0e8e58aa
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2022-08-01 12:03:20 -0400

    Carga de carpetas y configuración del archivo index

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de inventario</title>
-</head>
+<?php
+    include "./inc/head.php";
+?>
 <body>
-    
+    <?php
+        include "./inc/navbar.php";
+
+        include "./inc/script.php";
+    ?>
 </body>
 </html>
