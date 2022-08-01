ee69ac3c6076856861a86071bc504308ea0e4365
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2022-08-01 16:22:54 -0400

    archivos de funciones y respaldo de bd

---
+++ php/inventario.sql
@@ -0,0 +1,128 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-08-2022 a las 20:31:59
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `inventario`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `categoria_id` int(10) NOT NULL,
+  `categoria_nombre` varchar(100) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `categoria_ubicacion` varchar(100) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `producto_id` int(20) NOT NULL,
+  `producto_codigo` varchar(70) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `producto_nombre` varchar(70) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `producto_precio` int(100) NOT NULL,
+  `producto_stock` int(25) NOT NULL,
+  `producto_foto` varchar(500) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `categoria_id` int(10) NOT NULL,
+  `usuario_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `usuario_id` int(10) NOT NULL,
+  `usuario_nombre` varchar(40) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `usuario_apellido` varchar(40) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `usuario_usuario` varchar(20) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `usuario_clave` varchar(200) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `usuario_email` varchar(70) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`categoria_id`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`producto_id`),
+  ADD KEY `categoria_id` (`categoria_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`usuario_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `categoria_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `producto_id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `usuario_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`categoria_id`),
+  ADD CONSTRAINT `producto_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ php/main.php
@@ -0,0 +1,126 @@
+<?php
+/*Este archivo va a tener todas las funciones necesarias para el funcionamiento del proyecto,
+ desde la conexión y tdo el crud*/
+
+ //Conexión a la base de datos
+ function conexion(){
+    $pdo = new PDO('mysql:host=localhost;dbname=inventario', 'root', '');
+
+    return $pdo;
+ }
+
+ //Funcion para Verificar Datos
+function verificar_datos($filtro, $cadena){
+    if (preg_match("/^".$filtro."$/",$cadena)) {
+        return false;
+    }else{
+        return true;
+    }
+}
+
+//Función para evitar injección sql
+function limpiar_cadena($cadena){
+    $cadena = trim($cadena);
+    $cadena = stripslashes($cadena);
+    $cadena = str_ireplace("<script>","", $cadena);
+    $cadena = str_ireplace("</script>","", $cadena);
+    $cadena = str_ireplace("<script src>","", $cadena);
+    $cadena = str_ireplace("<script type=>","", $cadena);
+    $cadena = str_ireplace("SELECT * FROM","", $cadena);
+    $cadena = str_ireplace("DELETE FROM","", $cadena);
+    $cadena = str_ireplace("INSERT INTO","", $cadena);
+    $cadena = str_ireplace("DROP TABLE","", $cadena);
+    $cadena = str_ireplace("DROP DATABASE","", $cadena);
+    $cadena = str_ireplace("TRUNCATE TABLE","", $cadena);
+    $cadena = str_ireplace("SHOW TABLES","", $cadena);
+    $cadena = str_ireplace("SHOW DATABASES","", $cadena);
+    $cadena = str_ireplace("<?php","", $cadena);
+    $cadena = str_ireplace("?>","", $cadena);
+    $cadena = str_ireplace("--","", $cadena);
+    $cadena = str_ireplace("^","", $cadena);
+    $cadena = str_ireplace("<","", $cadena);
+    $cadena = str_ireplace(">","", $cadena);
+    $cadena = str_ireplace("!=","", $cadena);
+    $cadena = str_ireplace("&&","", $cadena);
+    $cadena = str_ireplace("||","", $cadena);
+    $cadena = str_ireplace("===","", $cadena);
+    $cadena = str_ireplace("==","", $cadena);
+    $cadena = str_ireplace(";","", $cadena);
+    $cadena = str_ireplace("::","", $cadena);
+    $cadena = str_ireplace("{","", $cadena);
+    $cadena = str_ireplace("}","", $cadena);
+    $cadena = str_ireplace("[","", $cadena);
+    $cadena = str_ireplace("]","", $cadena);
+    $cadena=trim($cadena);
+    $cadena=stripslashes($cadena);
+    return $cadena;
+}
+
+//funcion para re nombrar fotografias
+
+function renombrar_fotos($nombre){
+    $nombre=str_ireplace(" ","_",$nombre);
+    $nombre=str_ireplace("/","_",$nombre);
+    $nombre=str_ireplace("#","_",$nombre);
+    $nombre=str_ireplace("$","_",$nombre);
+    $nombre=str_ireplace(".","_",$nombre);
+    $nombre=str_ireplace(",","_",$nombre);
+    $nombre=$nombre."_".rand(1,100);
+
+    return $nombre;
+}
+
+//Función para paginación de las páginas
+function paginador_tablas($pagina, $Npaginas, $url, $botones){
+    $tabla='<nav class="pagination is-centered is-rounded" role="navigation" aria-label="pagination">';
+if ($pagina<=1) {
+    $tabla.='
+    <a class="pagination-previous is-disabled" disabled>Anterior</a>
+    <ul class="pagination-list">
+    ';
+}else{
+    $tabla.='
+    <a class="pagination-previous" href="'.$url.($pagina-1).'">Anterior</a>
+    <ul class="pagination-list">
+        <li><a class="pagination-link" href="'.$url.'1">1</a></li>
+        <li><span class="pagination-ellipsis">&hellip</span></li>
+    ';
+}
+
+$ci=0;
+for ($i=$pagina; $i <= $Npaginas; $i++) { 
+    if ($ci >=$botones) {
+        break;
+    }
+    if ($pagina==$i) {
+        $tabla.='<li><a class="pagination-link is-current" href="'.$url.$i.'">'.$i.'</a></li>';
+    } else {
+        $tabla.='<li><a class="pagination-link" href="'.$url.$i.'">'.$i.'</a></li>';
+    }
+
+    $ci++;
+    
+}
+
+
+if ($Npaginas==$Npaginas) {
+    $tabla.='
+    </ul>
+    <a class="pagination-next is-desabled" disabled>Siguiente</a>
+    ';
+}else{
+    $tabla.='
+        <li><span class="pagination-ellipsis">&hellip</span></li>
+        <li><a class="pagination-link" href="'.$url.$Npaginas.'">'.$Npaginas.'</a></li>
+    </ul>
+    <a class="pagination-next" href="'.$url.($pagina+1).'">Siguiente</a>
+    ';
+}
+
+
+    $tabla.='</nav>';
+
+    return $tabla;
+}
+
+
